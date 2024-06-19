f5dfb4823a5ad7a96807dac07d2fd57003b65f22
Dmitry Alexandrov <alexandrov.dm@mail.ru>
2024-06-19 07:32:46 +0300

    added urls, views

--- blogicum/pages/views.py
+++ blogicum/pages/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def about(request):
+    template = 'about/about.html'
+    return render(request, template)
+
+
+def rules(request):
+    template = 'about/rules.html'
+    return render(request, template)
